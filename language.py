4aee7deb3ef1f0f155717d733010385561a79989
rosemaryakl <aklrosemary1@gmail.com>
2021-07-22 11:53:45 +0300

    language as an input and get list of countries as output

--- language.py
+++ language.py
@@ -17,7 +17,7 @@ def generate_language_name_to_code(json_filepath):
 
     return output #''Xhosa': 'xh', 'Chinese': 'zh' 
 
-def generate_country_code_to_countries(json_filepath):
+def generate_language_code_to_countries(json_filepath):
     with open(json_filepath) as json_file:
         countries_arr = json.load(json_file)
 
@@ -35,10 +35,10 @@ def generate_country_code_to_countries(json_filepath):
         
     return output # the output dict will have a dict of key country code and value name of cities 'ZW': ['Zvishavane', 'Victoria Falls', 'Shurugwi', 'Shangani', 'Shamva', 'Rusape', 'Redcliff', 'Raffingora', 'Plumtree', 'Penhalonga', 'Odzi', 'Nyazura', 'Nyanga', 'Norton', 'Mvurwi', 'Mvuma', 'Mutoko', 'Mutare', 'Murehwa', 'Mount Darwin', 'Mhangura', 'Mazowe', 'Masvingo', 'Mashava', 'Marondera', 'Macheke', 'Lupane', 'Lalapanzi', 'Kwekwe', 'Karoi', 'Kariba', 'Kamativi Mine', 'Kadoma', 'Inyati', 'Insiza', 'Hwange', 'Headlands', 'Harare', 'Gweru', 'Gwanda', 'Gokwe', 'Glendale', 'Filabusi', 'Esigodini', 'Dorowa Mining Lease', 'Dete', 'Concession', 'Chivhu', 'Chirundu', 'Chiredzi', 'Chipinge', 'Chinhoyi', 'Chimanimani', 'Chegutu', 'Chakari', 'Centenary', 'Bulawayo', 'Binga', 'Bindura', 'Beitbridge', 'Beatrice', 'Banket', 'Epworth', 'Chitungwiza']
 
-language_code_to_cities_dict = generate_country_code_to_countries(LNG_JSON) # ex {'AD': ['Sant Julià de Lòria']} 
+language_code_to_cities_dict = generate_language_code_to_countries(LNG_JSON) # ex {'AD': ['Sant Julià de Lòria']} 
 language_name_to_code_dict = generate_language_name_to_code(COUNTRIES_JSON) # entire dict of name / code ex : 'South Africa': 'ZA', 'Zambia': 'ZM', 'Zimbabwe': 'ZW' }
 
-language_name_or_code = input('Enter a Country Name / Code: ')
+language_name_or_code = input('Enter a Language Name / Code: ')
 
 #success = True
 if language_name_or_code in language_code_to_cities_dict: # enter name or code both work to get output which is a list of cities
